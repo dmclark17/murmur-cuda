43a0d57fde69f93851002c3f4860527226e356df
David Clark <dmclark1729@gmail.com>
2019-07-23 10:05:00 -0400

    Adds checking using SMHASHER

--- main.cu
+++ main.cu
@@ -2,11 +2,12 @@
 #include <stdint.h>
 
 #include "murmuda3.h"
+#include "MurmurHash3.h"
 
 int main() {
 
-    int num_keys = 2;
-    int num_seeds = 2;
+    int num_keys = 5000;
+    int num_seeds = 500;
 
     int32_t* keys = new int32_t[num_keys];
     int len = sizeof(keys[0]);
@@ -23,10 +24,15 @@ int main() {
 
     MurmurHash3_batch(keys, len, num_keys, seeds, num_seeds, out);
 
-
+    // Check against the original C implementation from smhasher
+    uint32_t* c_out = new uint32_t[1];
     for (int i = 0; i < num_keys; i++) {
         for (int j = 0; j < num_seeds; j++) {
-            std::cout << out[j + i * num_seeds] << std::endl;
+            MurmurHash3_x86_32(&keys[i], len, seeds[j], c_out);
+            if (out[j + i * num_seeds] != *c_out) {
+                std::cout << "error for key:" << keys[i] << " seed:" <<
+                    seeds[j] << std::endl;
+            }
         }
     }
 }
